8dbae01dfac1a28433dda0314e7438012260fad9
soapmaze <lee141567@naver.com>
2016-12-02 12:06:02 +0900

    제시한 전 기능 구현 완료! 추후 생길 예외들을 고려하여 좀 더 상세하게 구현해볼 것.

--- app/src/main/java/com/example/soapmaze/android1202/MainActivity.java
+++ app/src/main/java/com/example/soapmaze/android1202/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.Chronometer;
 import android.widget.DatePicker;
 import android.widget.RadioButton;
@@ -18,9 +19,11 @@ public class MainActivity extends AppCompatActivity {
     Button b_start, b_end;
     RadioGroup rd;
     RadioButton rd_date, rd_time;
-    DatePicker d_pick;
+    CalendarView d_pick;
     TimePicker t_pick;
     TextView txt_reserving, txt_result;
+    String datetime;
+    String selected_date, selected_time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,23 +35,28 @@ public class MainActivity extends AppCompatActivity {
         rd = (RadioGroup)findViewById(R.id.rad_group);
         rd_date = (RadioButton)findViewById(R.id.radio_date);
         rd_time = (RadioButton)findViewById(R.id.radio_Time);
-        d_pick = (DatePicker)findViewById(R.id.datePicker);
+        d_pick = (CalendarView)findViewById(R.id.calendarView);
         t_pick = (TimePicker)findViewById(R.id.timePicker);
         txt_result = (TextView) findViewById(R.id.tview_result);
-        txt_reserving = (TextView)findViewById(R.id.tview_label);
+        rd.setVisibility(View.INVISIBLE);
         rd_date.setChecked(false);
         rd_time.setChecked(false);
         d_pick.setVisibility(View.INVISIBLE);
         t_pick.setVisibility(View.INVISIBLE);
         b_end.setVisibility(View.INVISIBLE);
+        datetime = "";
+        selected_date = "";
+        selected_time = "";
+
+
 
         b_start.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View view) {
                 r_time.start();
                 String strColor = "#FF0000";
-                txt_reserving.setTextColor(Color.parseColor(strColor));
                 r_time.setTextColor(Color.parseColor(strColor));
+                rd.setVisibility(View.VISIBLE);
             }
         });
 
@@ -60,11 +68,41 @@ public class MainActivity extends AppCompatActivity {
                         d_pick.setVisibility(View.VISIBLE);
                         t_pick.setVisibility(View.INVISIBLE);
                         b_end.setVisibility(View.VISIBLE);
+                        break;
                     case R.id.radio_Time :
-                        t_pick.setVisibility(View.VISIBLE);
                         d_pick.setVisibility(View.INVISIBLE);
+                        t_pick.setVisibility(View.VISIBLE);
                         b_end.setVisibility(View.VISIBLE);
+                        break;
                 }
+            }
+        });
+
+        d_pick.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView calendarView, int year, int month, int date) {
+                selected_date = year + "년 ";
+                selected_date += (month+1) + "월 ";
+                selected_date += date + "일 ";
+            }
+        });
+
+        t_pick.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker timePicker, int i, int i1) {
+                selected_time = i + "시 ";
+                selected_time += i1 + "분";
+            }
+        });
+
+        b_end.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                datetime = selected_date + selected_time;
+                txt_result.setText(datetime);
+                r_time.stop();
+                String strColor = "#0000FF";
+                r_time.setTextColor(Color.parseColor(strColor));
             }
         });
 
